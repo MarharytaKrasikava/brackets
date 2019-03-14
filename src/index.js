0d8ff7d632896dbcb6420bfc1e13ffeb8a1c7c1e
MarharytaKrasikava <marharytakrasikava@gmail.com>
2019-03-14 20:50:19 +0300

    added the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let s = 0; s < str.length; s++) {
+    for (let a = 0; a < bracketsConfig.length; a++) {
+        if ((str.charAt(s) == bracketsConfig[a][1]) && (bracketsConfig[a][0] != bracketsConfig[a][1])) {
+            let foundPos = str.indexOf(bracketsConfig[a][1], s);
+            if (str.charAt(foundPos - 1) == (bracketsConfig[a][0])) {
+                str = str.slice(0, foundPos - 1) + str.slice(foundPos + 1);
+                s = 0;
+            } else if (str.charAt(foundPos - 1) != (bracketsConfig[a][0])) 
+            {return false;}
+        } else if ((str.charAt(s) == bracketsConfig[a][1])&&(bracketsConfig[a][0] == bracketsConfig[a][1])&&(str.charAt(s)==str.charAt(s+1))) {
+            let foundPos = str.indexOf(bracketsConfig[a][0], s);
+            if (str.charAt(foundPos) == (bracketsConfig[a][1])) {
+                str = str.slice(0, foundPos) + str.slice(foundPos + 2);
+                s = 0;
+            } 
+        }
+    }
+}
+if (str == "") { return true; }
+else { return false; }
 }
